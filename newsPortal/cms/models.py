bb544ac0433aafba1a7012167a76d367497a1089
Anishmehta8210 <anishmehta7782@gmail.com>
2022-12-14 23:23:27 +0530

    first commit

---
+++ newsPortal/cms/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+# Create your models here.
+
+
+class Category(models.Model):
+    cat_title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.cat_title
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="images/",null=True,blank=True)
+    
+    def __str__(self):
+        return self.title
---
+++ newsPortal/cms/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render,redirect
+from .models import Post,Category
+# Create your views here.
+
+from .forms import PostForm
+def homepage(r):
+    data = {}
+    data['category'] = Category.objects.all()
+    data['posts'] = Post.objects.all()
+    return render(r,"home.html",data)
+
+
+def insert(r):
+    if r.method == "POST":
+        p = PostForm(r.POST,r.FILES)
+        if p.is_valid():
+            p.save()
+        return redirect(homepage)
+
+    return render(r,"insert.html",{"form":PostForm})
+
+
+def viewPost(r,cat_id):
+    data = {}
+    data['category'] = Category.objects.all()
+    data['posts'] = Post.objects.filter(category=cat_id)
+    return render(r,"home.html",data)
+
+
+# def search (r):
+#     data = {}
+#     search = r.GET.get("search")
+#     data['category'] = Category.objects.all()
+#     data['posts'] = Post.objects.filter(title__contains=search)
+#     return render(r,"home.html",data)
+
+
+def search(r):
+    search = r.GET.get("search")
+    data = {
+        "category":Category.objects.all(), 
+        "posts":Post.objects.filter(title__contains=search)
+        }
+    return render(r, "home.html",data)
